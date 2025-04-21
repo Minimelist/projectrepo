b4c7f5770bdb1bb1862edaf35c10091bf9454735
Minimelist <Melyap168@gmail.com>
2025-04-21 11:57:32 +0800

    remove conflict to show conflict

--- conflict.py
+++ conflict.py
@@ -4,5 +4,4 @@ age = "33"
 print(f"Hi, my name is {name} and I am {age} years old.")
 # This is a simple script that prints a greeting message with the user's name and age."
 # Attempt to create conflict with the main branch with age
-print(f"Hi, ")
-# Attempt to create conflict with the local main branch
+
