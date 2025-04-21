fe5844069ceb5f1f99e96ba9dedd21b4cd37bd75
MEL YAP <melyap168@gmail.com>
2025-04-21 11:26:30 +0800

    Conflict added for main to resolve

--- conflict.py
+++ conflict.py
@@ -3,3 +3,4 @@ age = "33"
 
 print(f"Hi, my name is {name} and I am {age} years old.")
 # This is a simple script that prints a greeting message with the user's name and age."
+# Attempt to create conflict with the main branch with age
